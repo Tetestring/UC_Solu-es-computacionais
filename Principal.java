b881a8a0af4375b61335c5aedf66488bb6c6acfb
Temeni Vieira <temenivieira24@gmail.com>
2024-10-25 20:41:22 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cartaodecretido c = new Cartaodecretido();
+        c.setCpf("12312352636");
+        c.setLimite(100);
+        Scanner sc = new Scanner(System.in);
+
+        int opcao;
+
+        do {
+            System.out.println("1 - Consultar Limite");
+            System.out.println("2 - Consultar Fatura");
+            System.out.println("3 - Realizar compra");
+            System.out.println("0 - Sair");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println(c.consultarLimite());
+                     break;
+                case 2:
+                    System.out.println(c.consultarTotalFatura());
+                    break;
+                case 3:
+                    System.out.println("Digite o valor da compra");
+                    double valor = sc.nextDouble();
+                    c.realizarCompra(valor);
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+
+            }
+     
+        } while (opcao != 0);
+        
+        sc.close();
+    }
+}
